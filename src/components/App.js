ff2022cb019fbc6c1d07eb2837209e37d1d259c9
Dipak1121 <dipaks9604@gmail.com>
2024-01-16 13:11:28 +0530

    thrid commit

--- src/components/App.js
+++ src/components/App.js
@@ -4,12 +4,11 @@ import './../styles/App.css';
 
 const App = ({temp, condition}) => {
   let divStyle = {
-    color: "white",
-    backgroundColor: "blue"
+    color: "blue",
   }
 
   if(temp > 20){
-    divStyle.backgroundColor = "red";
+    divStyle.color = "red";
   }
   return (
     <div style={divStyle}>
